a9334bd9b31e4558906bf5472cfb409a4ca9247e
frontendOne <leechenghua107@gmail.com>
2025-02-06 11:40:12 +0800

    新增product頁面

--- src/paths.ts
+++ src/paths.ts
@@ -3,6 +3,7 @@ export const paths = {
   auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password' },
   dashboard: {
     overview: '/dashboard',
+    product: '/dashboard/product',
     account: '/dashboard/account',
     customers: '/dashboard/customers',
     integrations: '/dashboard/integrations',
