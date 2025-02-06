6e5f9cc7c311ef750cfb63d2b14d0a21f45ea13e
frontendOne <leechenghua107@gmail.com>
2025-02-06 10:34:31 +0800

    側欄項目修改

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -2,10 +2,10 @@ import type { NavItemConfig } from '@/types/nav';
 import { paths } from '@/paths';
 
 export const navItems = [
-  { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
-  { key: 'customers', title: 'Customers', href: paths.dashboard.customers, icon: 'users' },
-  { key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
-  { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
-  { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
+  { key: 'overview', title: '商品列表', href: paths.dashboard.overview, icon: 'chart-pie' },
+  { key: 'customers', title: '會員列表', href: paths.dashboard.customers, icon: 'users' },
+  { key: 'integrations', title: '商品組合收藏', href: paths.dashboard.integrations, icon: 'plugs-connected' },
+  // { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
+  { key: 'account', title: '會員中心', href: paths.dashboard.account, icon: 'user' },
   { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
 ] satisfies NavItemConfig[];
